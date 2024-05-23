d4dcfa9848d47948697916d6d11ce0e5f741cb3a
Maksym <volomaksim72@gmail.com>
2024-05-23 18:44:10 +0100

    heheheh

--- max_hehba.js
+++ max_hehba.js
@@ -8,10 +8,10 @@ app.use(express.urlencoded({ extended: true }));
 const port = 3000
 
 router.get('/max', (req, res) => {
-    res.send("Hello? Can you hear me?")
+    return res.send("Hello? Can you hear me?")
 })
 router.get('/', (req, res) => {
-    res.redirect("https://youtu.be/dQw4w9WgXcQ?si=6h9x8RL3_JfnP125")
+    return res.redirect("https://youtu.be/dQw4w9WgXcQ?si=6h9x8RL3_JfnP125")
 })
 
 app.listen(port, ()=>{
